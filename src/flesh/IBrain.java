2f51607d0e57d52c7ad6053f59464429a01b374b
biospore <biospore@hackerdom.ru>
2015-09-21 18:37:07 +0500

    Changes in interfaces

--- src/flesh/IBrain.java
+++ src/flesh/IBrain.java
@@ -1,1 +1,39 @@
 package flesh;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
+import injuries.NoFlashbackFound;
+import injuries.NoPersonalityFound;
+import injuries.WrongType;
+import mind.IThought;
+import mind.IPersonality;
+import mind.IFlashback;
+import mind.cloth.ClothThought;
+
+public interface IBrain{
+    void addThought(String task, String type, String data);
+    Collection<?> getAllThoughts();
+
+    void addPersonality(String login, String password);
+    void deletePersonality(int id) throws NoPersonalityFound;
+    IPersonality getPersonality(int id) throws NoPersonalityFound;
+    Collection<?> getAllPersonalities();
+    void setActivePersonality(int id) throws NoPersonalityFound;
+    IPersonality getActivePersonality() throws NoPersonalityFound;
+
+    void addFlashback(IFlashback data) throws WrongType;
+    IFlashback getFlashback(long id, String type) throws NoFlashbackFound, WrongType;
+    void deleteFlashback(long id, String type) throws NoFlashbackFound, WrongType;
+    Collection<?> getAllFlashbacks(String type) throws WrongType;
+
+    Collection<?> searchFlashback(String regexp, String type) throws WrongType;
+    Collection<?> searchPersonality(String regexp);
+
+    void addRemoteFlashback(long id, String type);
+    void deleteRemoteFlashback(long id, String type);
+    void updateRemoteFlashback(long id, String type);
+    Collection<?> getAllRemoteFlashbacks(String type);
+    Collection<?> searchRemoteFlashback(String regexp, String type);
+}
--- src/mind/IThought.java
+++ src/mind/IThought.java
@@ -1,1 +1,14 @@
 package mind;
+
+import injuries.ThoughtWithoutTask;
+
+import java.io.Serializable;
+
+public interface IThought extends Serializable{
+    void setTask(String task);
+    String getTask() throws ThoughtWithoutTask;
+    void setType(String type);
+    String getType();
+    void setData(String data);
+    String getData();
+}
